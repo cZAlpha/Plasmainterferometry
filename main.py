a8c02b27cbb4d6fad8c2cfa5132a8d5554f8f5d5
cZAlpha <nbklaus21@students.desu.edu>
2024-07-01 13:55:37 -0700

    main.py comment changes

--- main.py
+++ main.py
@@ -5,11 +5,8 @@ from ui import *             # Import all user interface functions
 # Welcome!
 #
 # To analyze an image, follow the readMe directions for image preprocessing, then
-# replace the 'img_to_analyze' variable's string value to the relative path to
-# the image you'd like to analyze.
-#
-# After that, simply run this file and you will have the plot of the phase shift
-# displayed by the MatPlotLib library!
+# run this file to open the user interface. Select your file through the import button,
+# and specify what kind of analysis you want to be performed.
 #
 # To save your results, simply hit the save button on the plot window that pops up.
 #
